e15717914a87c693c7fb33bb9ab126f4b0eda7cd
Thilanka <thilanka@absi.be>
2014-03-06 16:31:44 +0530

    Add infra project to svn

---
+++ Infra/src/main/java/org/sixtynine/stock/App.java
@@ -0,0 +1,13 @@
+package org.sixtynine.stock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
