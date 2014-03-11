6472816a70978055683727c0653919f702d2f677
Thilanka <thilankadileepa@gmail.com>
2014-03-11 09:25:27 +0530

    initial changes

---
+++ Infra/src/test/java/org/sixtynine/stock/GeneralTest.java
@@ -0,0 +1,11 @@
+package org.sixtynine.stock;
+
+import junit.framework.TestCase;
+
+public class GeneralTest extends TestCase {
+	public void test()
+	{
+		System.out.println("Testing the application");
+		assertTrue(true);
+	}
+}
