49b6da0fe2e5702cd50db26090fe00c55fbb3cdd
Thilanka <thilankadileepa@gmail.com>
2014-03-16 10:00:22 +0530

    Create the web project

---
+++ StockScreener/src/main/java/org/sixtynine/stock/frontoffice/Home.java
@@ -0,0 +1,17 @@
+package org.sixtynine.stock.frontoffice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Home {
+
+	String message = "Welcome to your 1st Maven Spring project !";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage() {
+		System.out.println("from controller");
+		return new ModelAndView("hello", "message", message);
+	}
+}
