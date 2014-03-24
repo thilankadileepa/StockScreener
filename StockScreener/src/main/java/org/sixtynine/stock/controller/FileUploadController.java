29dc282307db4634465b9d09875d37a3d2aaaab0
Thilanka <thilankadileepa@gmail.com>
2014-03-24 06:30:36 +0530

    Jquery added

--- StockScreener/src/main/java/org/sixtynine/stock/controller/FileUploadController.java
+++ StockScreener/src/main/java/org/sixtynine/stock/controller/FileUploadController.java
@@ -19,55 +19,54 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 
-	 @RequestMapping(value = "/show", method = RequestMethod.GET)
-	    public String displayForm() {
-	        return "file_upload_form";
-	    }
-	     
-	    @RequestMapping(value = "/save", method = RequestMethod.POST)
-	    public String save(
-	            @ModelAttribute("uploadForm") FileUploadForm uploadForm,
-	                    Model map) {
-	        
-	    	InputStream inputStream = null;  
-	    	OutputStream outputStream = null;
-	    	  
-	        List<MultipartFile> files = uploadForm.getFiles();
-	 
-	        List<String> fileNames = new ArrayList<String>();
-	         
-	        if(null != files && files.size() > 0) {
-	            for (MultipartFile multipartFile : files) {
-	 
-	                String fileName = multipartFile.getOriginalFilename();
-	                fileNames.add(fileName);
-	                //Handle file content - multipartFile.getInputStream()
-	                
-	                try {  
-	                	   inputStream = multipartFile.getInputStream();  
-	                	  
-	                	   File newFile = new File("D:/CSE_ANALYZER/ANALYZER/upload/" + fileName);  
-	                	   if (!newFile.exists()) {  
-	                	    newFile.createNewFile();  
-	                	   }  
-	                	   outputStream = new FileOutputStream(newFile);  
-	                	   int read = 0;  
-	                	   byte[] bytes = new byte[1024];  
-	                	  
-	                	   while ((read = inputStream.read(bytes)) != -1) {  
-	                	    outputStream.write(bytes, 0, read);
-	                	    outputStream.close();
-	                	   }  
-	                	  } catch (IOException e) {  
-	                	   // TODO Auto-generated catch block  
-	                	   e.printStackTrace();  
-	                	  }  
-	 
-	            }
-	        }
-	         
-	        map.addAttribute("files", fileNames);
-	        return "file_upload_success";
-	  }
-	
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	public String displayForm() {
+		return "file_upload_form";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@ModelAttribute("uploadForm") FileUploadForm uploadForm,
+			Model map) {
+
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+
+		List<MultipartFile> files = uploadForm.getFiles();
+
+		List<String> fileNames = new ArrayList<String>();
+
+		if (null != files && files.size() > 0) {
+			for (MultipartFile multipartFile : files) {
+
+				String fileName = multipartFile.getOriginalFilename();
+				fileNames.add(fileName);
+				// Handle file content - multipartFile.getInputStream()
+
+				try {
+					inputStream = multipartFile.getInputStream();
+
+					File newFile = new File(fileName);
+					if (!newFile.exists()) {
+						newFile.createNewFile();
+					}
+					outputStream = new FileOutputStream(newFile);
+					int read = 0;
+					byte[] bytes = new byte[1024];
+
+					while ((read = inputStream.read(bytes)) != -1) {
+						outputStream.write(bytes, 0, read);
+						outputStream.close();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+		map.addAttribute("files", fileNames);
+		return "file_upload_success";
+	}
+
 }
